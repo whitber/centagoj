e9e29cd1b1dec0806c2e059a00c2af27c52a0341
Whit B <whitneyberard@gmail.com>
2021-05-08 23:10:33 -0400

    A first attempt at OAuth...

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask import render_template
+from flask_dance.consumer import OAuth2ConsumerBlueprint
 
+import os
 import requests
 import re
 import random
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # create the list of EO word/definitions
 page = requests.get("https://www.esperanto-panorama.net/vortaro/eo-en-u.htm")
@@ -14,6 +17,21 @@ soup = BeautifulSoup(page.content, 'html.parser')
 pre_sections = soup.find_all('pre')
 pre_texts = [pre.text for pre in pre_sections]
 all_words = [re.split(r'\s{2,}', row)  for pre in pre_texts for row in pre.split('\n')]
+
+
+clickup_client_id = os.environ.get('CLICKUP_CLIENT_ID')
+clickup_client_secret = os.environ.get('CLICKUP_CLIENT_SECRET')
+redirect_url = "http://127.0.0.1:5005/hillary.html"
+
+clickup_blueprint = OAuth2ConsumerBlueprint(
+    "clickup", __name__,
+    client_id=clickup_client_id,
+    client_secret=clickup_client_secret,
+    base_url="https://app.clickup.com",
+    token_url=f"https://app.clickup.com/api?client_id={clickup_client_id}&redirect_uri={redirect_url}",
+    authorization_url="",
+)
+app.register_blueprint(clickup_blueprint, url_prefix="/login")
 
 
 @app.route('/')
@@ -24,8 +42,14 @@ def index():
 
 @app.route('/hillary')
 def hillary():
+
+    #resp = clickup_blueprint.session.get("/user")
+    #assert resp.ok
+    #print("Here's the content of my response: " + resp.content)
+
     return render_template('hillary.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(port=5005, debug=True)
